27bfdeef4362caa41afc01d9fe95808e4c4613ca
jungbada269 <57929751+badasea@users.noreply.github.com>
2022-04-07 20:42:12 +0900

    타입스크립트 스터디 기초 문법 연습 commit
    타입스크립트 스터디 기초 문법 연습 commit

--- study/src/App.tsx
+++ study/src/App.tsx
@@ -1,22 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+// 변수 선언
+const areYouCool: boolean = true;
+const answer: number = 42;
+const typescript: string = "great";
+const greetings: string = `
+Hello, Readers!
+Welcome to TypeScript.
+`;
+const hasType: Object = {
+  TypeScript: true,
+  JavaScript: false,
+};
+
+// 배열 선언
+const pibonacci: Array<number> = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55];
+const myFavoriteBeers: Array<string> = [
+  "Imperial Stout",
+  "India Pale Ale",
+  "Weizenbock",
+];
+
+// 튜플
+const nameAndHeight: [string, number] = ["안희종", 176];
+
+// 객체
+const user: { name: string; height: number } = { name: "안희종", height: 176 };
 
 function App() {
+  // method
+  function sum(a: number, b: number): number {
+    return a + b;
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p>{greetings}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {typescript}, {pibonacci[0]}
         </a>
       </header>
     </div>
